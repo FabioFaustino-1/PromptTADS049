005085cafaf6c5ae5bb09c0015e5638147c5e21f
Angelo-msDev <angelo.mascarenhas.de.souza@gmail.com>
2025-10-02 18:20:36 -0300

    Update AltersTads.sql
    DE O PULL REQUEST MOH FILHOOOO

--- AltersTads.sql
+++ AltersTads.sql
@@ -1,38 +1,53 @@
--- Alterações na coluna Funcionarios
+-- Alterações na Tabela Funcionarios
+ALTER TABLE Funcionarios 
+  DROP COLUMN cargo;
 
-alter table funcionarios drop column cargo;
+ALTER TABLE Funcionarios 
+  ADD COLUMN cargo_Func VARCHAR(50) NOT NULL;
 
-alter table funcionarios add column cargo_Func varchar (50) not null;
+ALTER TABLE Funcionarios 
+  DROP COLUMN Rua;
 
-alter table funcionarios drop column Rua;
+ALTER TABLE Funcionarios 
+  ADD COLUMN bairro VARCHAR(50) NOT NULL;
 
-alter table funcionarios add column bairro varchar (50) not null;
+ALTER TABLE Funcionarios 
+  CHANGE COLUMN bairro bairro VARCHAR(50) AFTER nome;
 
-alter table funcionarios
-change column bairro bairro varchar(50) after nome;
+ALTER TABLE Funcionarios MODIFY telefone VARCHAR(20) NOT NULL;
 
--- Alterações na coluna Exemplares
-alter table Exemplares
-modify column `status` enum ("Pessimo", "Ruim", "Indiferente", "Bom", "Excelente") not null;
+-- Alterações na Tabela Exemplares
+ALTER TABLE Exemplares 
+  MODIFY COLUMN `status` ENUM("Pessimo", "Ruim", "Indiferente", "Bom", "Excelente") NOT NULL;
 
--- Alterações na coluna Clientes
-alter table clientes drop column Rua;
-
-alter table clientes add column bairro varchar (50) not null;
-
--- Alterações na tabela Departamentos
-
-alter table departamentos
-
-alter table departamentos change column Descricao_Ativ `status` enum ("Ativado", "Desativado") not null;
-
-alter table departamentos add column relatorio_mes varchar(100);
-
-
--- Alterações gerais
-ALTER TABLE Clientes MODIFY telefone VARCHAR(20) NOT NULL;
-ALTER TABLE funcionarios MODIFY telefone VARCHAR(20) NOT NULL;
-ALTER TABLE Livros MODIFY isbn varchar(20) primary key;
 ALTER TABLE Exemplares
-ADD COLUMN Livro_ISBN VARCHAR(13) NOT NULL,
-ADD CONSTRAINT fk_exemplar_livro FOREIGN KEY (Livro_ISBN) REFERENCES Livros(Isbn);
+ADD COLUMN livro_ISBN VARCHAR(13) NOT NULL,
+ADD CONSTRAINT fk_exemplar_livro FOREIGN KEY (livro_ISBN) REFERENCES Livros(ISBN);
+
+-- Alterações na Tabela Clientes
+ALTER TABLE Clientes 
+  DROP COLUMN Rua;
+
+ALTER TABLE Clientes 
+  ADD COLUMN bairro VARCHAR(50) NOT NULL;
+
+ALTER TABLE Clientes MODIFY telefone VARCHAR(20) NOT NULL;
+
+-- Alterações na Tabela Departamentos
+ALTER TABLE Departamentos 
+  CHANGE COLUMN Descricao_Ativ `status` ENUM("Ativado", "Desativado") NOT NULL;
+
+ALTER TABLE Departamentos 
+  ADD COLUMN relatorio_mes VARCHAR(100) NOT NULL;
+
+-- Alteração da Tabela Pedidos
+ALTER TABLE Pedidos 
+  MODIFY COLUMN `status` ENUM("pedido efetuado", "pagamento confirmado", "pedido separado", "saiu para entrega", "entregue");
+
+-- Alteração da Tabela Livros 
+ALTER TABLE Livros DROP COLUMN Genero;
+
+ALTER TABLE Livros MODIFY ISBN VARCHAR(20) PRIMARY KEY NOT NULL;
+
+-- Alteração da Tabela Identificadores 
+ALTER TABLE Identificadores ADD COLUMN genero VARCHAR(20) NOT NULL;
