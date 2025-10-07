02e6312658252fbc289cc5186a82e1412a1493af
FabioFaustino-1 <fabiofaustino010@gmail.com>
2025-10-07 16:25:56 -0300

    Update AltersTads.sql

--- AltersTads.sql
+++ AltersTads.sql
@@ -16,24 +16,28 @@ alter table Exemplares
 modify column `status` enum ("Pessimo", "Ruim", "Indiferente", "Bom", "Excelente") not null;
 
 -- Alterações na coluna Clientes
-alter table clientes drop column Rua;
+alter table clientes drop column `Rua`;
 
-alter table clientes add column Cep_clie varchar(9) not null;
+alter table clientes add column `Cep_clie` varchar(9) not null;
 
 
 -- Alterações na tabela Departamentos
 
 alter table departamentos;
 
-alter table departamentos change column descricao_Ativ descricao_Ativ varchar(250) not null;
+alter table departamentos change column `descricao_Ativ` `descricao_Ativ` varchar(250) not null;
 
-alter table departamentos add column relatorio_mes varchar(100);
+alter table departamentos add column `relatorio_mes` varchar(100);
 
 
 -- Alterações gerais
-ALTER TABLE Clientes MODIFY telefone VARCHAR(20) NOT NULL;
-ALTER TABLE funcionarios MODIFY telefone VARCHAR(20) NOT NULL;
-ALTER TABLE Livros MODIFY isbn varchar(20);
-ALTER TABLE Exemplares
-ADD COLUMN Livro_ISBN VARCHAR(20) NOT NULL,
-ADD CONSTRAINT fk_exemplar_livro FOREIGN KEY (Livro_ISBN) REFERENCES Livros(Isbn);
+ALTER TABLE Clientes MODIFY `telefone` VARCHAR(20) NOT NULL;
+ALTER TABLE funcionarios MODIFY `telefone` VARCHAR(20) NOT NULL;
+ALTER TABLE Livros MODIFY `isbn` varchar(20);
+/*ALTER TABLE Exemplares
+ADD COLUMN Livro_ISBN VARCHAR(20) References Livros(isbn),
+ADD CONSTRAINT fk_exemplar_livro FOREIGN KEY (Livro_ISBN) REFERENCES Livros(Isbn);*/
+
+
+
+
